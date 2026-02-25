a0dd9c1de14aacb55345e9496d15e68133b4f228
Mario Navarrete <mfnavarrete3@gmail.com>
2026-02-25 15:51:55 -0500

    environment comunicacion localhost -> dev.icsvial.com

--- frt_icsvial/src/app/constants/api.constants.ts
+++ frt_icsvial/src/app/constants/api.constants.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = '';
+import { environment } from '../../environments/environment';
+
+export const API_BASE_URL = environment.apiBaseUrl;
 export const API_SERVICIOS_PATH = '/api/servicios';
 export const API_CLIENTES_PATH = '/api/clientes';
 export const API_BLOGS_PATH = '/api/blogs';
